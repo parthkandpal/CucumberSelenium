cf75530fc89614b6b01b95f703e25f664de62970
Parth <parth.kandpal#gmail.com>
2019-11-01 22:49:14 +0530

    added hooks feature and step definition for global hooks

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -7,13 +7,13 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="C:\\Users\\PARTH KANDPAL\\eclipse-workspace\\BDDFramework\\src\\main\\java\\Features\\Tags.feature",  
+		features="C:\\Users\\PARTH KANDPAL\\eclipse-workspace\\BDDFramework\\src\\main\\java\\Features\\hooks.feature",  
 		glue={"stepDefinition"},			//package name for stepdefinitions 
 		plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-report.json","junit:target/junit.xml" },
 		monochrome = true,
 		strict= true,
-		dryRun= false,
-		tags = {"@End2End" ,"@RegressionTest"}
+		dryRun= false
+//		tags = {"@End2End" ,"@RegressionTest"}
 		
 		//ORed : tags = {"@SmokeTest , @RegressionTest"} -- execute all tests tagged as @SmokeTest OR @RegressionTest  both in a single ""
 		//ANDed : tags = tags = {"@SmokeTest" , "@RegressionTest"} -- execute all tests tagged as @SmokeTest AND @RegressionTest both in separate ""
---
+++ target/cucumber-html-report/report.js
@@ -0,0 +1,270 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/PARTH KANDPAL/eclipse-workspace/BDDFramework/src/main/java/Features/hooks.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM app test",
+  "description": "",
+  "id": "free-crm-app-test",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 630746,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "free crm create deal test",
+  "description": "",
+  "id": "free-crm-app-test;free-crm-create-deal-test",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "user is on deal page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "user fills the deals form",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "deal is created",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_is_on_deal_oage()"
+});
+formatter.result({
+  "duration": 501450664,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_fills_the_deals_form()"
+});
+formatter.result({
+  "duration": 149590,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.deal_is_created()"
+});
+formatter.result({
+  "duration": 1131950,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 354184,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 6297692,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 9,
+  "name": "free crm create contact test",
+  "description": "",
+  "id": "free-crm-app-test;free-crm-create-contact-test",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 10,
+  "name": "user is on contact page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "user fills the contact form",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "contact is created",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_is_on_contact_page()"
+});
+formatter.result({
+  "duration": 1576094,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_fills_the_contact_form()"
+});
+formatter.result({
+  "duration": 174778,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.contact_is_created()"
+});
+formatter.result({
+  "duration": 106923,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 90474,
+  "status": "passed"
+});
+formatter.scenarioOutline({
+  "line": 15,
+  "name": "free crm create mail test",
+  "description": "",
+  "id": "free-crm-app-test;free-crm-create-mail-test",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 16,
+  "name": "user is on mail page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "user fills the mail form",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 18,
+  "name": "mail is created",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 19,
+  "name": "",
+  "description": "",
+  "id": "free-crm-app-test;free-crm-create-mail-test;",
+  "rows": [
+    {
+      "cells": [
+        "mail1"
+      ],
+      "line": 20,
+      "id": "free-crm-app-test;free-crm-create-mail-test;;1"
+    },
+    {
+      "cells": [
+        "mail2"
+      ],
+      "line": 21,
+      "id": "free-crm-app-test;free-crm-create-mail-test;;2"
+    },
+    {
+      "cells": [
+        "mail3"
+      ],
+      "line": 22,
+      "id": "free-crm-app-test;free-crm-create-mail-test;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 587051,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 21,
+  "name": "free crm create mail test",
+  "description": "",
+  "id": "free-crm-app-test;free-crm-create-mail-test;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 16,
+  "name": "user is on mail page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "user fills the mail form",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 18,
+  "name": "mail is created",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_is_on_mail_page()"
+});
+formatter.result({
+  "duration": 524851,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_fills_the_mail_form()"
+});
+formatter.result({
+  "duration": 373204,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.mail_is_created()"
+});
+formatter.result({
+  "duration": 313574,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 345445,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 646167,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 22,
+  "name": "free crm create mail test",
+  "description": "",
+  "id": "free-crm-app-test;free-crm-create-mail-test;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 16,
+  "name": "user is on mail page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "user fills the mail form",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 18,
+  "name": "mail is created",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_is_on_mail_page()"
+});
+formatter.result({
+  "duration": 88418,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.user_fills_the_mail_form()"
+});
+formatter.result({
+  "duration": 289413,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HooksStepDefinition.mail_is_created()"
+});
+formatter.result({
+  "duration": 282730,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 5285516,
+  "status": "passed"
+});
+});
